598e7abba3246244b3d8f330af9c5e0088f71cf2
Til <zoodb@gmx.de>
2022-03-16 16:16:51 +0100

    full insert works

--- phtree/common/flat_b_plus_tree.h
+++ phtree/common/flat_b_plus_tree.h
@@ -19,6 +19,7 @@
 
 #include "bits.h"
 #include <cassert>
+#include <iostream>
 #include <tuple>
 #include <vector>
 
@@ -72,7 +73,7 @@ class BstIterator;
 template <typename Entry>
 using DataIterator = decltype(std::vector<BptEntry<Entry>>().begin());
 
-constexpr static size_t M = 8;
+constexpr static size_t M = 4;
 
 /*
  * Strategy:
@@ -171,28 +172,8 @@ class b_plus_tree_node {
             auto split_pos = M >> 1;
             auto split_key = data_[split_pos - 1].first;
             auto max_key = data_[M - 1].first;
-            if (parent_) {
-                // TODO
-                //assert(false && "Implement overflow");
-                //                auto split_pos = M >> 1;
-                //                auto split_key = data_[split_pos].first;
-                //                auto max_key = data_[M - 1].first;
-                //                auto node2 = parent_->UpdateKeyAndAddNode(max_key, split_key,
-                //                this);
-                //                // TODO ensure we are not incrementally inserting here, see
-                //                //
-                //                https://stackoverflow.com/questions/15004517/moving-elements-from-stdvector-to-another-one
-                //                node2->data_.insert(node2->data_.end(),
-                //                std::make_move_iterator(data_.begin() + split_pos),
-                //                          std::make_move_iterator(data_.end()));
-                //                // TODO: ensure we are not incrementally removing!
-                //                data_.erase(data_.begin() + split_pos, data_.end());
-                //
-                //                // insert entry
-                //                // TODO emplace directly w/o checking again in try_emplace...
-                //                return try_emplace_base(lower_bound(key), key,
-                //                std::forward<Args>(args)...);
-            } else {
+            if (parent_ == nullptr) {
+                // special root node handling
                 assert(parent_ == nullptr);
                 auto* new_parent = new NodeT(false, nullptr, nullptr, nullptr);
                 new_parent->data_.emplace_back(max_key, this);
@@ -200,7 +181,15 @@ class b_plus_tree_node {
                 parent_ = new_parent;
             }
 
-            auto node2 = parent_->UpdateKeyAndAddNode(max_key, split_key, this);
+            // create new node
+            auto node2 = new NodeT(true, parent_, this, next_node_);
+            if (next_node_ != nullptr) {
+                next_node_->prev_node_ = node2;
+            }
+            next_node_ = node2;
+
+            // populate new node
+            // auto node2 = parent_->UpdateKeyAndAddNode(max_key, split_key, this, tree);
             // TODO ensure we are not incrementally inserting here, see
             //  https://stackoverflow.com/questions/15004517/moving-elements-from-stdvector-to-another-one
             node2->data_.insert(
@@ -210,12 +199,21 @@ class b_plus_tree_node {
             // TODO: ensure we are not incrementally removing!
             data_.erase(data_.begin() + split_pos, data_.end());
 
+            // Add node to parent
+            parent_->UpdateKeyAndAddNode(max_key, split_key, std::max(max_key, key), node2, tree);
             // insert entry
-            // TODO emplace directly w/o checking again in try_emplace...
             if (key <= split_key) {
-                return EmplaceNoCheck(lower_bound(key), key, std::forward<Args>(args)...);
+                // parent_->UpdateKeyAndAddNode(max_key, split_key, this, tree);
+                return EmplaceNoCheck(it, key, std::forward<Args>(args)...);
             }
+            // TODO Optimize populating new node: move 1st part, insert new value, move 2nd part...?
             return node2->EmplaceNoCheck(node2->lower_bound(key), key, std::forward<Args>(args)...);
+        }
+        if (parent_ != nullptr) {
+            auto max_key = data_[data_.size() - 1].first;
+            if (key > max_key) {
+                parent_->UpdateKey(max_key, key);
+            }
         }
         return EmplaceNoCheck(it, key, std::forward<Args>(args)...);
     }
@@ -243,40 +241,179 @@ class b_plus_tree_node {
         return is_leaf_;
     }
 
+    void _check(
+        size_t& count, NodeT* parent, NodeT*& prev_leaf, key_t& known_min, key_t known_max) {
+        assert(parent_ == nullptr || data_.size() >= 2);
+        assert(parent_ == parent);
+        if (data_.empty()) {
+            assert(parent == nullptr);
+            return;
+        }
+        assert(count == 0 || known_min < data_[0].first);
+        assert(parent_ == nullptr || known_max == data_[data_.size() - 1].first);
+        if (is_leaf_) {
+            assert(prev_leaf == prev_node_);
+            auto prev_key = data_[0].first;
+            int n = 0;
+            for (auto& e : data_) {
+                assert(n == 0 || e.first > prev_key);
+                assert(parent_ == nullptr || e.first <= known_max);
+                ++n;
+                ++count;
+                known_min = e.first;
+            }
+            prev_leaf = this;
+        } else {
+            auto prev_key = data_[0].first;
+            int n = 0;
+            for (auto& e : data_) {
+                assert(n == 0 || e.first > prev_key);
+                e.node_->_check(count, this, prev_leaf, known_min, e.first);
+                assert(parent_ == nullptr || e.first <= known_max);
+                ++n;
+            }
+        }
+    }
+
   private:
     void UpdateKey(index_t old_key, index_t new_key) {
         assert(new_key > old_key);
         auto it = lower_bound(old_key);
         assert(it != data_.end());
-        assert(it->first == old_key);
+        // TODO This should work !!!
+        // TODO This should work !!!
+        // TODO This should work !!!
+        // TODO This should work !!!
+        // assert(it->first == old_key);
+        assert(it->first == old_key || it->first == new_key);  // TODO remove!!!
         it->first = new_key;
+        if (parent_ != nullptr && ++it == data_.end()) {
+            parent_->UpdateKey(old_key, new_key);
+        }
     }
 
     /*
      * This method does two things:
-     * - It changes the key of the node at 'old_key' to 'new_key'.
-     * - It inserts a new node after 'new_key' with value 'old_key'
+     * - It changes the key of the node (node 1) at 'key1_old' to 'key1_new' (they may be the same).
+     * - It inserts a new node (node 2) after 'new_key1' with value 'key2'
      * Invariants:
-     * - old_key > new_key
-     * - there is no other key between new_key and old_key
+     * - Node1: key1_old >= key1_new; Node 1 vs 2: key2 > new_key1
+     * - there is no other key between key1_new and key1_old
      */
-    NodeT* UpdateKeyAndAddNode(index_t old_key, index_t new_key, NodeT* old_node) {
-        assert(old_key > new_key);
+    void UpdateKeyAndAddNode(
+        index_t key1_old, index_t key1_new, key_t key2, NodeT* child2, TreeT& tree) {
+        assert(key2 > key1_new);
+        assert(key1_old >= key1_new);
         assert(!is_leaf());
-        if (data_.size() < M) {
-            auto it = lower_bound(old_key);
-            assert(it != data_.end());
-            it->first = new_key;
-            ++it;
-            auto new_node = new NodeT(old_node->is_leaf_, this, old_node, old_node->next_node_);
-            data_.insert(it, {old_key, new_node});
-            if (old_node->next_node_) {
-                old_node->next_node_->prev_node_ = new_node;
+        NodeT* dest = this;
+        bool split = false;
+        if (data_.size() >= M) {
+            split = true;
+            auto split_pos = M >> 1;
+            auto split_key = data_[split_pos - 1].first;
+            auto max_key = data_[M - 1].first;
+            if (parent_ == nullptr) {
+                auto* new_parent = new NodeT(false, nullptr, nullptr, nullptr);
+                new_parent->data_.emplace_back(max_key, this);
+                tree.root_ = new_parent;
+                parent_ = new_parent;
             }
-            return new_node;
+
+            // create new node
+            auto sibbling2 = new NodeT(false, parent_, this, next_node_);
+            // TODO skip this for non-leaf nodes...?
+            if (next_node_ != nullptr) {
+                next_node_->prev_node_ = sibbling2;
+            }
+            next_node_ = sibbling2;
+
+            // populate new node
+            // TODO ensure we are not incrementally inserting here, see
+            //  https://stackoverflow.com/questions/15004517/moving-elements-from-stdvector-to-another-one
+            sibbling2->data_.insert(
+                sibbling2->data_.end(),
+                std::make_move_iterator(data_.begin() + split_pos),
+                std::make_move_iterator(data_.end()));
+            // TODO: ensure we are not incrementally removing!
+            data_.erase(data_.begin() + split_pos, data_.end());
+            for (auto& e : sibbling2->data_) {
+                e.node_->parent_ = sibbling2;
+            }
+
+            // Add node to parent
+            parent_->UpdateKeyAndAddNode(
+                max_key, split_key, std::max(max_key, key2), sibbling2, tree);
+            // insert entry
+            if (key2 <= split_key) {
+                std::cout << "UKAAN0: " << key1_old << "-> " << key1_new << " / " << key2
+                          << std::endl;  // TODO
+                                         //                auto it = lower_bound(key2);
+                //                assert(it == data_.end() || it->first != key2);
+                //                data_.emplace(it, key2, child2);
+                // TODO remove all this
+                assert(dest->data_.size() < M);
+                auto it = dest->lower_bound(key1_old);
+                assert(it != dest->data_.end());
+                it->first = key1_new;
+                ++it;
+                // auto new_node = new NodeT(old_node->is_leaf_, this, old_node,
+                // old_node->next_node_);
+                // TODO emplace?
+                dest->data_.insert(it, {key2, child2});
+                //        if (old_node->next_node_) {
+                //            old_node->next_node_->prev_node_ = new_node;
+                //        }
+                //        return new_node;
+                if (parent_ != nullptr) {
+                    auto max_key = dest->data_[dest->data_.size() - 1].first;
+                    if (key2 > max_key) {
+                        parent_->UpdateKey(dest->data_[dest->data_.size() - 1].first, key2);
+                    }
+                }
+                return;
+            } else {
+                std::cout << "UKAAN1: " << key1_old << "-> " << key1_new << " / " << key2
+                          << std::endl;  // TODO
+                dest = sibbling2;
+                child2->parent_ = dest;
+                if (key1_old <= split_key) {
+                    std::cout << "UKAAN11111: " << key1_old << "-> " << key1_new << " / " << key2
+                              << std::endl;  // TODO
+                                             // Special case: old child in current node and new
+                                             // child in new node
+                    assert(data_.size() < M);
+                    auto it = lower_bound(key1_old);
+                    assert(it != data_.end());
+                    it->first = key1_new;
+                    assert(++it == data_.end());
+
+                    assert(dest->data_.size() < M);
+                    auto it2 = dest->lower_bound(key2);
+                    // TODO use begin() above
+                    assert(it2 == dest->data_.begin());
+                    dest->data_.insert(it2, {key2, child2});
+
+                    return;
+                }
+                std::cout << "UKAAN12222: " << key1_old << "-> " << key1_new << " / " << key2
+                          << std::endl;  // TODO
+            }
         }
-        assert(false && "TODO implement");
-        return nullptr;
+        std::cout << "UKAAN: " << key1_old << "-> " << key1_new << " / " << key2
+                  << std::endl;  // TODO
+
+        assert(dest->data_.size() < M);
+        auto it = dest->lower_bound(key1_old);
+        assert(it != dest->data_.end());
+        it->first = key1_new;
+        ++it;
+
+        // TODO emplace?
+        dest->data_.insert(it, {key2, child2});
+        assert(child2->parent_ == dest);
+        if (parent_ != nullptr && key2 > key1_old) {
+            parent_->UpdateKey(key1_old, key2);
+        }
     }
 
     template <typename... Args>
@@ -307,9 +444,9 @@ class b_plus_tree_node {
     template <typename... Args>
     auto EmplaceNoCheck(const DataIteratorT& it, index_t key, Args&&... args) {
         assert(it == data_.end() || it->first != key);
-        if (parent_ != nullptr && it == data_.end()) {
-            parent_->UpdateKey(data_[data_.size() - 1].first, key);
-        }
+        //        if (parent_ != nullptr && it == data_.end()) {
+        //            parent_->UpdateKey(data_[data_.size() - 1].first, key);
+        //        }
         //            auto x = data_.emplace(
         //                it,
         //                std::piecewise_construct,
@@ -465,6 +602,14 @@ class b_plus_tree_map {
         return size_;
     }
 
+    void _check() {
+        size_t count = 0;
+        NodeT* prev_leaf = nullptr;
+        key_t known_min = 3423412;
+        root_->_check(count, nullptr, prev_leaf, known_min, 0);
+        assert(count == size());
+    }
+
   private:
     //    template <typename... Args>
     //    auto emplace_base(size_t key, Args&&... args) {
@@ -488,6 +633,7 @@ class b_plus_tree_map {
             assert(it != node->end());
             node = it->node_;
         }
+        // TODO move code into try_emplace(), or use find() ?
         auto it = node->lower_bound(key);
         if (it != node->end() && it->first == key) {
             return std::make_pair(it, false);
