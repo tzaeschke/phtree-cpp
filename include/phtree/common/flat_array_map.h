1a8676cf79a74a4d2b337dbbb433ab113472c928
tzaeschke <zoodb@gmx.de>
2023-02-09 19:04:12 +0100

    initial

--- include/phtree/common/flat_array_map.h
+++ include/phtree/common/flat_array_map.h
@@ -31,7 +31,7 @@
  */
 namespace improbable::phtree {
 
-template <typename Key, typename Value, std::size_t SIZE>
+template <typename Key, typename Value, Key SIZE>
 class flat_array_map;
 
 namespace detail {
@@ -39,14 +39,14 @@ namespace detail {
 template <typename Key, typename Value>
 using flat_map_pair = std::pair<Key, Value>;
 
-template <typename Key, typename Value, std::size_t SIZE>
+template <typename Key, typename Value, Key SIZE>
 class flat_map_iterator {
     friend flat_array_map<Key, Value, SIZE>;
 
   public:
     flat_map_iterator() : first{0}, map_{nullptr} {};
 
-    explicit flat_map_iterator(size_t index, const flat_array_map<Key, Value, SIZE>* map)
+    explicit flat_map_iterator(Key index, const flat_array_map<Key, Value, SIZE>* map)
     : first{index}, map_{map} {
         assert(index <= SIZE);
     }
@@ -66,7 +66,7 @@ class flat_map_iterator {
         return *this;
     }
 
-    auto operator++(int) const noexcept {
+    auto operator++(int) noexcept {
         flat_map_iterator it(first, map_);
         ++(*this);
         return it;
@@ -81,7 +81,7 @@ class flat_map_iterator {
     }
 
   private:
-    size_t first;
+    Key first;
     const flat_array_map<Key, Value, SIZE>* map_;
 };
 }  // namespace detail
@@ -93,18 +93,20 @@ class flat_map_iterator {
  * It has O(1) insertion/removal time complexity, but O(2^DIM) space complexity, so it is best used
  * when DIM is low and/or the map is known to have a high fill ratio.
  */
-template <typename Key, typename Value, std::size_t SIZE>
+template <typename Key, typename Value, Key SIZE>
 class flat_array_map {
+    static_assert(std::is_integral<Key>() && "Key type must be integer");
+    static_assert(std::is_unsigned<Key>() && "Key type must unsigned");
     using map_pair = detail::flat_map_pair<Key, Value>;
     using iterator = detail::flat_map_iterator<Key, Value, SIZE>;
     friend iterator;
 
   public:
-    [[nodiscard]] auto find(size_t index) noexcept {
+    [[nodiscard]] auto find(Key index) noexcept {
         return iterator{occupied(index) ? index : SIZE, this};
     }
 
-    [[nodiscard]] auto lower_bound(size_t index) const noexcept {
+    [[nodiscard]] auto lower_bound(Key index) const noexcept {
         return iterator{lower_bound_index(index), this};
     }
 
@@ -122,7 +124,7 @@ class flat_array_map {
 
     ~flat_array_map() noexcept {
         if (occupancy != 0) {
-            for (size_t i = 0; i < SIZE; ++i) {
+            for (Key i = 0; i < SIZE; ++i) {
                 if (occupied(i)) {
                     data(i).~pair();
                 }
@@ -131,11 +133,13 @@ class flat_array_map {
     }
 
     [[nodiscard]] size_t size() const noexcept {
-        return std::bitset<64>(occupancy).count();
+        constexpr size_t BITS =
+            std::numeric_limits<Key>::digits + std::numeric_limits<Key>::is_signed;
+        return std::bitset<BITS>(occupancy).count();
     }
 
     template <typename... Args>
-    std::pair<map_pair*, bool> try_emplace(size_t index, Args&&... args) {
+    std::pair<map_pair*, bool> try_emplace(Key index, Args&&... args) {
         if (!occupied(index)) {
             new (reinterpret_cast<void*>(&data_[index])) map_pair(
                 std::piecewise_construct,
@@ -147,7 +151,7 @@ class flat_array_map {
         return {&data(index), false};
     }
 
-    bool erase(size_t index) noexcept {
+    bool erase(Key index) noexcept {
         if (occupied(index)) {
             data(index).~pair();
             unoccupy(index);
@@ -164,38 +168,38 @@ class flat_array_map {
     /*
      * This returns the element at the given index, which is _not_ the n'th element (for n = index).
      */
-    map_pair& data(size_t index) noexcept {
+    map_pair& data(Key index) noexcept {
         assert(occupied(index));
         return *std::launder(reinterpret_cast<map_pair*>(&data_[index]));
     }
 
-    const map_pair& data(size_t index) const noexcept {
+    const map_pair& data(Key index) const noexcept {
         assert(occupied(index));
         return *std::launder(reinterpret_cast<const map_pair*>(&data_[index]));
     }
 
-    [[nodiscard]] size_t lower_bound_index(size_t index) const noexcept {
+    [[nodiscard]] Key lower_bound_index(Key index) const noexcept {
         assert(index < SIZE);
-        size_t num_zeros = CountTrailingZeros(occupancy >> index);
+        Key num_zeros = CountTrailingZeros(occupancy >> index);
         // num_zeros may be equal to SIZE if no bits remain
         return std::min(SIZE, index + num_zeros);
     }
 
-    void occupy(size_t index) noexcept {
+    void occupy(Key index) noexcept {
         assert(index < SIZE);
         assert(!occupied(index));
         // flip the bit
         occupancy ^= (Key{1} << index);
     }
 
-    void unoccupy(size_t index) noexcept {
+    void unoccupy(Key index) noexcept {
         assert(index < SIZE);
         assert(occupied(index));
         // flip the bit
         occupancy ^= (Key{1} << index);
     }
 
-    [[nodiscard]] bool occupied(size_t index) const noexcept {
+    [[nodiscard]] bool occupied(Key index) const noexcept {
         return (occupancy >> index) & Key{1};
     }
 
@@ -209,7 +213,7 @@ class flat_array_map {
  * This is useful to decouple instantiation of a node from instantiation of it's descendants
  * (the flat_array_map directly instantiates an array of descendants).
  */
-template <typename Key, typename Value, std::size_t SIZE>
+template <typename Key, typename Value, Key SIZE>
 class array_map {
     static_assert(SIZE <= 64);  // or else we need to adapt 'occupancy'
     static_assert(SIZE > 0);
@@ -237,15 +241,15 @@ class array_map {
         delete data_;
     }
 
-    [[nodiscard]] auto find(size_t index) noexcept {
+    [[nodiscard]] auto find(Key index) noexcept {
         return data_->find(index);
     }
 
-    [[nodiscard]] auto find(size_t key) const noexcept {
+    [[nodiscard]] auto find(Key key) const noexcept {
         return const_cast<array_map&>(*this).find(key);
     }
 
-    [[nodiscard]] auto lower_bound(size_t index) const {
+    [[nodiscard]] auto lower_bound(Key index) const {
         return data_->lower_bound(index);
     }
 
@@ -267,17 +271,17 @@ class array_map {
     }
 
     template <typename... Args>
-    auto try_emplace(size_t index, Args&&... args) {
+    auto try_emplace(Key index, Args&&... args) {
         return data_->try_emplace(index, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
-    auto try_emplace(const iterator&, size_t index, Args&&... args) {
+    auto try_emplace(const iterator&, Key index, Args&&... args) {
         // We ignore the iterator, this is an array based collection, so access is ~O(1).
         return data_->try_emplace(index, std::forward<Args>(args)...).first;
     }
 
-    bool erase(size_t index) {
+    bool erase(Key index) {
         return data_->erase(index);
     }
 
