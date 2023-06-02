5e99d2865a74918c0f2a7ee1454ef4b17a2406b2
tzaeschke <zoodb@gmx.de>
2023-06-02 18:23:47 +0200

    apple clang

--- include/phtree/common/bits.h
+++ include/phtree/common/bits.h
@@ -34,99 +34,99 @@
  */
 namespace improbable::phtree::detail {
 
-//namespace {
-//inline bit_width_t CountLeadingZeros(std::uint64_t bit_string) {
-//    if (bit_string == 0) {
-//        return 64;
-//    }
-//    bit_width_t n = 1;
-//    std::uint32_t x = (bit_string >> 32);
-//    if (x == 0) {
-//        n += 32;
-//        x = (int)bit_string;
-//    }
-//    if (x >> 16 == 0) {
-//        n += 16;
-//        x <<= 16;
-//    }
-//    if (x >> 24 == 0) {
-//        n += 8;
-//        x <<= 8;
-//    }
-//    if (x >> 28 == 0) {
-//        n += 4;
-//        x <<= 4;
-//    }
-//    if (x >> 30 == 0) {
-//        n += 2;
-//        x <<= 2;
-//    }
-//    n -= x >> 31;
-//    return n;
-//}
+// namespace {
+// inline bit_width_t CountLeadingZeros(std::uint64_t bit_string) {
+//     if (bit_string == 0) {
+//         return 64;
+//     }
+//     bit_width_t n = 1;
+//     std::uint32_t x = (bit_string >> 32);
+//     if (x == 0) {
+//         n += 32;
+//         x = (int)bit_string;
+//     }
+//     if (x >> 16 == 0) {
+//         n += 16;
+//         x <<= 16;
+//     }
+//     if (x >> 24 == 0) {
+//         n += 8;
+//         x <<= 8;
+//     }
+//     if (x >> 28 == 0) {
+//         n += 4;
+//         x <<= 4;
+//     }
+//     if (x >> 30 == 0) {
+//         n += 2;
+//         x <<= 2;
+//     }
+//     n -= x >> 31;
+//     return n;
+// }
 //
-//inline bit_width_t CountLeadingZeros(std::uint32_t bit_string) {
-//    if (bit_string == 0) {
-//        return 32;
-//    }
-//    bit_width_t n = 1;
-//    if (bit_string >> 16 == 0) {
-//        n += 16;
-//        bit_string <<= 16;
-//    }
-//    if (bit_string >> 24 == 0) {
-//        n += 8;
-//        bit_string <<= 8;
-//    }
-//    if (bit_string >> 28 == 0) {
-//        n += 4;
-//        bit_string <<= 4;
-//    }
-//    if (bit_string >> 30 == 0) {
-//        n += 2;
-//        bit_string <<= 2;
-//    }
-//    n -= bit_string >> 31;
-//    return n;
-//}
+// inline bit_width_t CountLeadingZeros(std::uint32_t bit_string) {
+//     if (bit_string == 0) {
+//         return 32;
+//     }
+//     bit_width_t n = 1;
+//     if (bit_string >> 16 == 0) {
+//         n += 16;
+//         bit_string <<= 16;
+//     }
+//     if (bit_string >> 24 == 0) {
+//         n += 8;
+//         bit_string <<= 8;
+//     }
+//     if (bit_string >> 28 == 0) {
+//         n += 4;
+//         bit_string <<= 4;
+//     }
+//     if (bit_string >> 30 == 0) {
+//         n += 2;
+//         bit_string <<= 2;
+//     }
+//     n -= bit_string >> 31;
+//     return n;
+// }
 //
-//inline bit_width_t CountTrailingZeros(std::uint64_t bit_string) {
-//    if (bit_string == 0) {
-//        return 64;
-//    }
-//    uint32_t x = 0;
-//    uint32_t y = 0;
-//    uint16_t n = 63;
-//    y = (std::uint32_t)bit_string;
-//    if (y != 0) {
-//        n = n - 32;
-//        x = y;
-//    } else {
-//        x = (std::uint32_t)(bit_string >> 32);
-//    }
-//    y = x << 16;
-//    if (y != 0) {
-//        n = n - 16;
-//        x = y;
-//    }
-//    y = x << 8;
-//    if (y != 0) {
-//        n = n - 8;
-//        x = y;
-//    }
-//    y = x << 4;
-//    if (y != 0) {
-//        n = n - 4;
-//        x = y;
-//    }
-//    y = x << 2;
-//    if (y != 0) {
-//        n = n - 2;
-//        x = y;
-//    }
-//    return n - ((x << 1) >> 31);
-//}
-//}
+// inline bit_width_t CountTrailingZeros(std::uint64_t bit_string) {
+//     if (bit_string == 0) {
+//         return 64;
+//     }
+//     uint32_t x = 0;
+//     uint32_t y = 0;
+//     uint16_t n = 63;
+//     y = (std::uint32_t)bit_string;
+//     if (y != 0) {
+//         n = n - 32;
+//         x = y;
+//     } else {
+//         x = (std::uint32_t)(bit_string >> 32);
+//     }
+//     y = x << 16;
+//     if (y != 0) {
+//         n = n - 16;
+//         x = y;
+//     }
+//     y = x << 8;
+//     if (y != 0) {
+//         n = n - 8;
+//         x = y;
+//     }
+//     y = x << 4;
+//     if (y != 0) {
+//         n = n - 4;
+//         x = y;
+//     }
+//     y = x << 2;
+//     if (y != 0) {
+//         n = n - 2;
+//         x = y;
+//     }
+//     return n - ((x << 1) >> 31);
+// }
+// }
 
 #if defined(__clang__) || defined(__GNUC__)
 // See https://en.cppreference.com/w/cpp/language/types
@@ -261,17 +261,16 @@ inline bit_width_t CountTrailingZeros(std::uint64_t bit_string) {
 }
 #endif
 
-
-//#if defined(__clang__) || defined(__GNUC__)
-//#define CountLeadingZeros(bits) NumberOfLeadingZeros_GCC_BUILTIN(bits)
-//#define CountTrailingZeros(bits) NumberOfTrailingZeros_GCC_BUILTIN(bits)
-//#elif defined(_MSC_VER)
-//#define CountLeadingZeros(bits) NumberOfLeadingZeros_MSVC_BUILTIN(bits)
-//#define CountTrailingZeros(bits) NumberOfTrailingZeros_MSVC_BUILTIN(bits)
-//#else
-//#define CountLeadingZeros(bits) NumberOfLeadingZeros(bits)
-//#define CountTrailingZeros(bits) NumberOfTrailingZeros(bits)
-//#endif
+// #if defined(__clang__) || defined(__GNUC__)
+// #define CountLeadingZeros(bits) NumberOfLeadingZeros_GCC_BUILTIN(bits)
+// #define CountTrailingZeros(bits) NumberOfTrailingZeros_GCC_BUILTIN(bits)
+// #elif defined(_MSC_VER)
+// #define CountLeadingZeros(bits) NumberOfLeadingZeros_MSVC_BUILTIN(bits)
+// #define CountTrailingZeros(bits) NumberOfTrailingZeros_MSVC_BUILTIN(bits)
+// #else
+// #define CountLeadingZeros(bits) NumberOfLeadingZeros(bits)
+// #define CountTrailingZeros(bits) NumberOfTrailingZeros(bits)
+// #endif
 
 }  // namespace improbable::phtree::detail
 
