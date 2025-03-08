e72ab282a37d0e7e51c257682d605d2aacb80550
tzaeschke <zoodb@gmx.de>
2025-03-08 18:50:50 +0100

    Release 1.6.2

--- test/phtree_test_issue_153.cc
+++ test/phtree_test_issue_153.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Tilmann Zäschke
+ * Copyright 2025 Tilmann Zäschke
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
